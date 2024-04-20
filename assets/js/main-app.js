1a6a12d970dcf246d6c5adf161ec15f6aed73fc3
Mohamed Alnawjha <m.nawjha@gmail.com>
2024-04-20 13:31:03 +0300

    last edit in quran app

--- assets/js/main-app.js
+++ assets/js/main-app.js
@@ -1,4 +1,4 @@
-// scroll to top
+// Scroll To Top
 const handelScrollBtn = () => {
   let toTopBtn = document.querySelector('#to-top');
   const onScrollDisplayBtn = () => {
@@ -19,3 +19,50 @@ const handelScrollBtn = () => {
   toTopBtn.addEventListener('click', onClickScrollToTop);
 };
 handelScrollBtn();
+
+//Dark Mode
+const handelDarkMode = () => {
+  let darkBtn = document.querySelector('#dark-toggle');
+  let bodyEle = document.body;
+  if (darkBtn) {
+    darkBtn.addEventListener('click', () => {
+      bodyEle.classList.toggle('dark-theme');
+    });
+  }
+};
+handelDarkMode();
+
+//Full Page
+const handelFullPage = () => {
+  let fullPageBtn = document.querySelectorAll('.full-toggle');
+  let bodyEle = document.body;
+  fullPageBtn.forEach(item => {
+    item.addEventListener('click', () => {
+      bodyEle.classList.toggle('full-page');
+    });
+  });
+};
+handelFullPage();
+
+// Quran Paragraph Font Size
+const handelFontSize = () => {
+  let quranPar = document.querySelector('.quran-par');
+  let btnSizeMinus = document.querySelector('.btn-size-minus');
+  let btnSizePlus = document.querySelector('.btn-size-plus');
+  let fontSize = parseInt(quranPar.style.fontSize);
+  let minSize = 18;
+  let maxSize = 40;
+  const handelDecrease = () => {
+    if (fontSize > minSize) {
+      quranPar.style.fontSize = `${--fontSize}px`;
+    }
+  };
+  const handelIncrease = () => {
+    if (fontSize < maxSize) {
+      quranPar.style.fontSize = `${++fontSize}px`;
+    }
+  };
+  btnSizeMinus.addEventListener('click', handelDecrease);
+  btnSizePlus.addEventListener('click', handelIncrease);
+};
+handelFontSize();
